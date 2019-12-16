7c3345c896b88a00e82dff22b53190339a30908a
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-12-16 20:44:16 +0200

    Update Program.cs

--- sqlite2/Program.cs
+++ sqlite2/Program.cs
@@ -17,6 +17,16 @@ namespace _1612
 
             List<Employee> employees = CompanyDAO.GetEmployees();
             employees.ForEach(e => Console.WriteLine(e));
+            
+            // Language Integrated Query (LINQ)
+           //employees.Where(e => e.AGE > 20).ToList().ForEach(e => Console.WriteLine(e));
+
+            var result = from e in employees
+                                     where e.AGE > 20
+                                     orderby e.AGE
+                                     select e;
+
+            result.ToList().ForEach(e => Console.WriteLine(e));
         }
     }
 }
